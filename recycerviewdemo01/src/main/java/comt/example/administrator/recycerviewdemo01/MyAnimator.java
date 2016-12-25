c9507e553ed531af6444c6221c4ba42fba579bbd
zhao6429598 <348088053@qq.com>
2016-12-26 00:54:34 +0800

    Initial commit

---
+++ recycerviewdemo01/src/main/java/comt/example/administrator/recycerviewdemo01/MyAnimator.java
@@ -0,0 +1,12 @@
+package comt.example.administrator.recycerviewdemo01;
+
+/**
+ * Author: zwf(zhaoweifeng@1000phone.com)
+ * Date  : 2016-12-26
+ * Time  : 00:22
+ * Project: AndroidMaterialDesign
+ * Descrite:
+ */
+
+public class MyAnimator {
+}
